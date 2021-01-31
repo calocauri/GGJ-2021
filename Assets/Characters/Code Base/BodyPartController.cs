33b3d73a2079e57782a08ed41793dfdcc6e011f4
calocauri <calocauri@gmail.com>
2021-01-31 16:29:06 -0600

    Nest script & bodyparts

--- Assets/Characters/Code Base/BodyPartController.cs
+++ Assets/Characters/Code Base/BodyPartController.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 
 public class BodyPartController : MonoBehaviour, Connectable {
 	private new Rigidbody rigidbody;
+	private SpringJoint joint;
 
 	ConnectableType Connectable.connectableType => ConnectableType.BodyPart;
 	Rigidbody Connectable.rigidbody => rigidbody;
 
 	private void Awake() {
 		rigidbody = GetComponent<Rigidbody>();
+		joint = GetComponent<SpringJoint>();
+		joint.connectedAnchor = transform.position;
 	}
 
 	private void FixedUpdate()
 	{
 		rigidbody.AddForce(-Physics.gravity);
+	}
 
+	public void AttachTo(Rigidbody rigidbody)
+	{
+		if (rigidbody)
+		{
+			joint.connectedBody = rigidbody;
+			joint.connectedAnchor = Vector3.zero;
+		}
+		else
+		{
+			joint.connectedBody = null;
+			joint.connectedAnchor = transform.position;
+		}
+	}
+
+	public void AttachTo(Vector3 worldAnchor)
+	{
+		joint.connectedBody = null;
+		joint.connectedAnchor = worldAnchor;
 	}
 }
--- Assets/Interactables/Scripts/Nest.cs
+++ Assets/Interactables/Scripts/Nest.cs
@@ -2,15 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Interactables;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Nest : MonoBehaviour
 {
-    private PlayerController _player;
-    [SerializeField][ContextMenuItem("Create Slots", "CreateSlots")] private int requiredParts;
+    
+    [FormerlySerializedAs("_player")] [SerializeField] private PlayerController player;
+    [SerializeField] private int requiredParts;
     private Vector3[] _slotPositions;
+    [SerializeField] float increment = 0.5f;
+    [SerializeField] private List<BodyPartController> bodyParts = new List<BodyPartController>();
+    [SerializeField] private Activation activation;
 
     private void Awake()
+    {
+        CreateSlots();
+    }
+    private void OnValidate()
     {
         CreateSlots();
     }
@@ -18,40 +28,41 @@ public class Nest : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         if (!other.transform.gameObject.CompareTag("Player")) return;
-        if ( _player = other.transform.gameObject.GetComponent<PlayerController>() )
-        {
-            //TODO: Get children and put inside nest, then activate if children == requiredParts
-        }
-        else
-        {
-            Debug.LogWarning($"Could not find component \"PlayerController\"", other.transform.gameObject);
-        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        _player = null;
+        if (!other.transform.gameObject.CompareTag("Player")) return;
+        Debug.Log(bodyParts.Count);
+        player.RecoverBodyparts(other.transform.gameObject.GetComponent<Rigidbody>(), ref bodyParts);
     }
-    
-    //TODO: subscribe to children pick-up action
-    public void OnChildrenPickup(PlayerController player)
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (!other.transform.gameObject.CompareTag("Player")) return;
+        var body = other.transform.GetComponent<Rigidbody>();
+        player.ReleaseBodyParts(body, this);
+    }
+
+    public void Attach(BodyPartController bodyPart)
+    {
+        bodyPart.AttachTo(_slotPositions[bodyParts.Count]);
+        bodyParts.Add(bodyPart);
+        Debug.Log(bodyParts.Count);
+        if (bodyParts.Count == requiredParts) activation.Run();
     }
 
     private void CreateSlots()
     {
-        var increment = 0.5f;
         _slotPositions = new Vector3[requiredParts];
         for (int i = requiredParts - 1; i >= 0; i--)
         {
             _slotPositions[i] = transform.TransformPoint(new Vector3(0f, increment * i, 0f));
-            
         }
     }
     
     private void OnDrawGizmos()
     {
-        if (_slotPositions != null) foreach (var slot in _slotPositions.ToList()) Gizmos.DrawSphere(slot, 0.1f);
+        if (_slotPositions != null) foreach (var slot in _slotPositions.ToList()) Gizmos.DrawSphere(slot, 0.2f);
     }
 }
