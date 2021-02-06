23a69355001895a1205c43e04a7b3ea7e145ef6b
josueor <jortigoza@dreidel.mx>
2021-02-06 13:31:01 -0600

    Camara v2, parece que ya es funcional.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -11,35 +11,62 @@ public class CameraFollow : MonoBehaviour
     private Vector3 targetPos;
     private Vector3 middlePos;
     public float height = 0;
-
+    public GameObject Player1;
+    public GameObject Player2;
     private Vector3 middlePoint;
-
     private float distance;
-    private float cameraSize = 4.88f;
-
-
-
+    private float cameraSize = 6;
+    private bool combinated;
+    private bool player1active;
+    private bool player2active;
     private void Start()
     {
         MainCamera = GetComponent<Camera>();
         cameraPos = camTransform.position;
-        targetPos = Target1.position;  
+        targetPos = Target1.position;
+        combinated = false;
     }
 
      private void Update() {
          
 
-        if(distance > 16.9f){     
-        MainCamera.orthographicSize = Mathf.Lerp(MainCamera.orthographicSize, 8.5f, 0.1f);
+        if(distance > 9.9f && !combinated){     
+        MainCamera.orthographicSize = Mathf.Lerp(MainCamera.orthographicSize, 8.5f, 0.005f);
         } else {
-            MainCamera.orthographicSize = Mathf.Lerp(MainCamera.orthographicSize, cameraSize, 0.1f);
+            MainCamera.orthographicSize = Mathf.Lerp(MainCamera.orthographicSize, cameraSize, 0.005f);
         }
-
 
          middlePoint = (Target1.position + Target2.position) / 2;
         targetPos = Target1.position;
-        cameraPos = new Vector3 (middlePoint.x, middlePoint.y + height, -20);
+
+        if(Player1.activeInHierarchy == false && Player2.activeInHierarchy == true){
+
+            combinated = true;
+            middlePoint = Target2.position;
+            cameraPos = new Vector3 (middlePoint.x, middlePoint.y + height, -20);
+            transform.position = cameraPos;  
+
+        }else {
+            cameraPos = new Vector3 (middlePoint.x, middlePoint.y + height, -20);
+            transform.position = cameraPos;  
+        };
+
+           if(Player2.activeInHierarchy == false && Player1.activeInHierarchy ==true){
+
+            combinated = true;
+            middlePoint = Target1.position;
+            cameraPos = new Vector3 (middlePoint.x, middlePoint.y + height, -20);
+            transform.position = cameraPos;  
+
+        }else {
+            cameraPos = new Vector3 (middlePoint.x, middlePoint.y + height, -20);
         transform.position = cameraPos;  
+
+        };
+
+        if (Player1.activeInHierarchy && Player2.activeInHierarchy){
+             combinated = false;
+        };
 
         //Calcular distancia entre los objetos
         distance = Vector3.Distance(Target1.position, Target2.position);
@@ -53,6 +80,7 @@ public class CameraFollow : MonoBehaviour
          Gizmos.DrawSphere(middlePoint, 0.3f);
 
     }
+
 
 
 }
